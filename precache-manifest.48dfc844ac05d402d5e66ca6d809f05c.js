17fe2540752adec7c8ed587987053f602c34a835
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-08-25 19:38:44 -0500

    Updates

---
+++ precache-manifest.48dfc844ac05d402d5e66ca6d809f05c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4b2b28cced6516e43c33d33ae0ba91",
+    "url": "/brainf/index.html"
+  },
+  {
+    "revision": "1c4de987ec2f6791073f",
+    "url": "/brainf/static/css/2.f75db26e.chunk.css"
+  },
+  {
+    "revision": "56570edcb62258f67418",
+    "url": "/brainf/static/css/main.f7ef897d.chunk.css"
+  },
+  {
+    "revision": "1c4de987ec2f6791073f",
+    "url": "/brainf/static/js/2.7b876e4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/brainf/static/js/2.7b876e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56570edcb62258f67418",
+    "url": "/brainf/static/js/main.a62450d6.chunk.js"
+  },
+  {
+    "revision": "dd4c8783ea2ebff3faf1",
+    "url": "/brainf/static/js/runtime-main.02090f13.js"
+  }
+]);
